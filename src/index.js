6f03903f72f2b2740fee231a2b9ebb8b6c21fd41
alyszys <wojtkowskaalicja@gmail.com>
2021-07-07 15:42:45 +0200

    Setting up HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import _ from 'lodash';
-import './style.css';
+/*import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 import Notes from './data.csv';
 import toml from './data.toml';
 import yaml from './data.yaml';
-import json from './data.json5';
+import json from './data.json5';*/
+import printMe from './print.js';
 
-console.log(toml.title); // output `TOML Example`
+/*console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
 
 console.log(yaml.title); // output `YAML Example`
 console.log(yaml.owner.name); // output `Tom Preston-Werner`
 
 console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+console.log(json.owner.name); // output `Tom Preston-Werner`*/
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
+ /* // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   
@@ -30,7 +32,12 @@ function component() {
   element.appendChild(myIcon);
   
   console.log(Data);
-  console.log(Notes);
+  console.log(Notes); */
+  
+  tn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
