6ea739b12cd27a9a9e08b922e2a5cff53bd1b48f
alyszys <wojtkowskaalicja@gmail.com>
2021-07-16 14:42:30 +0200

    przycisk do przewijania

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./style.scss"
 console.log('index.js loaded');
+
+$(document).ready(function(){ 
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#button').fadeIn();
+        } else {
+            $('#button').fadeOut();
+        }
+    }); 
+
+    $('#button').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
+
+});
